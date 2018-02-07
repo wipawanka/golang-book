a866102662dda10f78d9f3d901233d5ee10c093e
Wipawan.ka <wipawan.ka@tnis.com>
2018-02-07 11:01:11 +0700

    edit main.go

--- chapter3-3/main.go
+++ chapter3-3/main.go
@@ -7,4 +7,13 @@ func main() {
 	third := 1.0/3.0
 	fmt.Printf("third =%v\n",third)
 	fmt.Printf("third + third + third =%v\n",third+third+third)
+
+	fmt.Println("=====Comparing floating point=====")
+	fmt.Println("0.1 + 0.2 == 0.3 is ", 0.1 +0.2 == 0.3)
+	num :=0.1
+	fmt.Println("num is", num)
+	num +=0.2
+	fmt.Println("num is", num)
+	fmt.Println("num == 0.3 is", num == 0.3)
+	fmt.Println("num is", num)
 }
